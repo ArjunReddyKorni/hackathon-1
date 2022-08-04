c9cdc83c437471505660b5514c1a90389419b2d7
863978_Korni <arjunreddykorni@gmail.com>
2022-08-04 07:11:08 +0000

    Saving changes from StackRoute workbench on Thu Aug 04 2022 12:41:06 GMT+0530 (India Standard Time)

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -9,9 +9,13 @@ public class SortingCities
     public static void main( String[] args )
     {
         SortingCities object = new SortingCities();
-		String [] cities = {Bern,Lucerne,Interlaken,Grindelwald,Engelberg,Geneva,Murren,Basel,Zermatt,Jungfraujoch}
-    }
-	public  void sorting(String []cites){
+		String [] cities = {Bern,Lucerne,Interlaken,Grindelwald,Engelberg,Geneva,Murren,     Basel,Zermatt,Jungfraujoch}
+		int [] distance = {138,52,118,136,85,276,103,87,214,101}
 
+    }
+	public  String[] toUpperCase(String []cites){
+			String [] upperCase = new String[cites.length];
+
+			for(int i =0;i < cites.length;i++)
 	}
 }
