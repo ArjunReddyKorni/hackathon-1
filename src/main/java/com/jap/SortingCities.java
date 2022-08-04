3ab218989dbe67dc16b37de13c13920623c9e4bd
863978_Korni <arjunreddykorni@gmail.com>
2022-08-04 08:03:34 +0000

    Saving changes from StackRoute workbench on Thu Aug 04 2022 13:33:33 GMT+0530 (India Standard Time)

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -12,7 +12,7 @@ public class SortingCities
 		String [] cities = {"Bern","Lucerne","Interlaken","Grindelwald","Engelberg","Geneva","Murren","Basel","Zermatt","Jungfraujoch"};
 		int [] distance = {138,52,118,136,85,276,103,87,214,101};
 
-		String [] upperCase = object.toUpperCase(cities);
+		String [] upperCase = object.toUpperCasee(cities);
 		for(int i =0;i< cities.length;i++){
 				System.out.println(upperCase[i]+ ", ");
 		}
@@ -20,11 +20,11 @@ public class SortingCities
 		object.greaterCites(distance,cities);
 
     }
-	public String[] toUpperCase(String []cites){
+	public String[] toUpperCasee(String []cities){
 
-			String [] upperCase = new String[cites.length];
+			String [] upperCase = new String[cities.length];
 
-			for(int i =0;i < cites.length;i++){
+			for(int i =0;i < cities.length;i++){
 				upperCase[i] = cities[i].toUpperCase;
 
 			}
@@ -40,7 +40,7 @@ public class SortingCities
 	}
 	public void nearZurich(int [] distance, String []cities){
 
-			for(int i=0;i < cities.lenth;i++){
+			for(int i=0;i < cities.length;i++){
 
 				if(distance[i] == 52 )
 				System.out.println(cities[i]);
